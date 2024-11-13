1a12930046ca8d2e943a7a3125db33c21c92f7f5
Olha-la3 <olialia5@rambler.ru>
2024-11-13 09:18:00 +0300

    Add files via upload

---
+++ tests_12_1.py
@@ -0,0 +1,30 @@
+from runner import Runner
+import unittest
+
+
+class RunnerTest(unittest.TestCase):
+
+    def test_walk(self):
+        runner = Runner('Masha')
+        for _ in range(10):
+            runner.walk()
+        self.assertEqual(runner.distance,50)
+
+    def test_run(self):
+        runner = Runner('John')
+        for _ in range(10):
+            runner.run()
+        self.assertEqual(runner.distance, 100)
+
+    def test_challenge(self):
+        runner1 = Runner('John')
+        runner2 = Runner('Max')
+        for _ in range(10):
+            runner1.walk()
+            runner2.run()
+
+        self.assertNotEqual(runner1.distance, runner2.distance)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests_12_2.py
@@ -0,0 +1,66 @@
+import unittest
+from runner_and_tournament import Runner
+from runner_and_tournament import Tournament
+
+class TournamentTest(unittest.TestCase):
+
+    # def setUp(self):
+    #     runner1 = Runner('Усэйн', 10)
+    #     runner2 = Runner('Андрей', 9)
+    #     runner3 = Runner('Ник', 3)
+    #
+    # @classnethod
+    # def setUpClass(cls):
+    #     cls.all_results = {}
+    #
+    # def tearDownClass(self):
+    #     for i in cls.all_results:
+    #         print(i, cls.all_results)
+    #
+    # def testTournament(self):
+    #     tournament = Tournament(90)
+    #     cls.all_results = tournament.start()
+    #
+    #     self.assetTrue(sorted(cls.all_results[-1]), 'Ник')
+
+    def setUp(cls):
+        cls.usain = Runner('Усэйн', 10)
+        cls.Andrei = Runner('Андрей', 9)
+        cls.Nik = Runner('Ник', 3)
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    @classmethod
+    def tearDownClass(cls):
+        for i in cls.all_results:
+            print(cls.all_results[i])
+
+    def testTournament1(cls):
+        tournament = Tournament(90, cls.usain, cls.Nik)
+        dict1 = tournament.start()
+        for i in dict1:
+            dict1[i] = dict1[i].name
+        cls.all_results[1] = dict1
+        cls.assertTrue(dict1[2], 'Ник')
+
+    def testTournament2(cls):
+        tournament = Tournament(90, cls.Andrei, cls.Nik)
+        dict1 = tournament.start()
+        for i in dict1:
+            dict1[i] = dict1[i].name
+        cls.all_results[2] = dict1
+        cls.assertTrue(dict1[2], 'Ник')
+
+    def testTournament3(cls):
+        tournament = Tournament(90, cls.usain, cls.Andrei, cls.Nik)
+        dict1 = tournament.start()
+        for i in dict1:
+            dict1[i] = dict1[i].name
+        cls.all_results[3] = dict1
+        cls.assertTrue(dict1[2], 'Ник')
+
+
+if __name__ == '__main__':
+    unittest.main()
