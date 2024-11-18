613bb81f5878cb0797054763727767a8da78c0a9
Olha-la3 <olialia5@rambler.ru>
2024-11-18 21:15:19 +0300

    Update 111_12.py

--- 111_12.py
+++ 111_12.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api = "7616859155:AAEORIw8E63b3WJy_WPk5ucXgGhmfqQ6Ues"
+api = 
 bot = Bot(token = api)
 dp = Dispatcher(bot, storage= MemoryStorage())
 
